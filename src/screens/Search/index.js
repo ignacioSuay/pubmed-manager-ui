c5f9fc80c938420a03ec8a07df0ba2dbf0434c47
Ignacio Suay <ignacio.suay@gmail.com>
2018-06-13 17:20:18 +0100

    Improve ui

--- src/screens/Search/index.js
+++ src/screens/Search/index.js
@@ -1,27 +1,29 @@
 import React from 'react';
-import {Button, Keyboard, StyleSheet, Text, TextInput, TouchableWithoutFeedback, View} from 'react-native';
-import filterConfig from '../../config/filters.config';
+import {Button, Keyboard, StyleSheet, TouchableWithoutFeedback, View, TouchableHighlight} from 'react-native';
+import {Hoshi} from 'react-native-textinput-effects';
+import filterConfig from '../../config/filters.config'
 
 
 export default class Search extends React.PureComponent {
 
-    state = {};
+    state = {
+        term: {},
+        showBasicFilters: false
+    };
 
     constructor(props) {
         super(props);
     };
 
     setUserProps(key, value) {
-        this.setState({
-            [key]: value
-        });
+        this.setState({term: Object.assign({}, this.state.term, {[key]: value})});
     }
 
     buildTerm() {
         let res = "";
 
-        Object.entries(this.state).forEach(entry => {
-            if(entry[1]) {
+        Object.entries(this.state.term).forEach(entry => {
+            if (entry[1]) {
                 res += entry[1] + entry[0] + " AND ";
             }
         });
@@ -32,9 +34,11 @@ export default class Search extends React.PureComponent {
 
     search() {
         const searchTerm = this.buildTerm();
-        console.log("RESULTADO " + searchTerm);
-
         this.props.navigation.navigate('Results', {term: searchTerm});
+    }
+
+    showFilters(visibility) {
+        this.setState({showBasicFilters: visibility});
     }
 
     render() {
@@ -42,33 +46,46 @@ export default class Search extends React.PureComponent {
             <TouchableWithoutFeedback onPress={Keyboard.dismiss} accessible={false}>
 
                 <View style={styles.container}>
-                    <View style={styles.top}>
-                        <TextInput style={styles.input} onChangeText={value => {
-                            this.setUserProps("[All fields]", value)
-                        }} placeholder={"Search..."}/>
+
+                    <View style={styles.input}>
+                        <Hoshi label={'Search...'} borderColor={'#2b80c4'}
+                               onChangeText={value => this.setUserProps("[All fields]", value)}/>
                     </View>
 
-                    <View>
-                        {
-                            filterConfig.map((filter, index) => (
-                                <View key={index}>
-                                    <Text>{filter.name}</Text>
-                                    <TextInput style={styles.input} placeholder={filter.name} onChangeText={value => {
-                                        this.setUserProps(filter.filter, value)
-                                    }}/>
-                                </View>
-                            ))
-                        }
+                    <View style={styles.filtersButtons}>
+                        {!this.state.showBasicFilters && <Button title="+ Filters" onPress={() => this.showFilters(true)}/>}
+                        {this.state.showBasicFilters && <Button style={styles.minusFilter} color={'#ff3246'} title="- Filters" onPress={()=> this.showFilters(false)}/>}
                     </View>
 
-                    <View style={styles.bottom}>
+                    <View style={styles.search}>
                         <Button title="Search" onPress={this.search.bind(this)}/>
                     </View>
+
+                    {this.renderFilters()}
 
                 </View>
             </TouchableWithoutFeedback>
 
         );
+    }
+
+    renderFilters() {
+        if (this.state.showBasicFilters) {
+            return (
+                <View style={styles.basicFilters}>
+                    {
+                        filterConfig.map((filter, index) => (
+                            <View key={index} style={styles.input}>
+                                <Hoshi
+                                    label={filter.name} borderColor={'#2b80c4'}
+                                    onChangeText={value => this.setUserProps(filter.filter, value)}/>
+                            </View>))
+                    }
+                </View>
+
+            )
+        }
+
     }
 
     static navigationOptions = {
@@ -83,25 +100,34 @@ const styles = StyleSheet.create({
     container: {
         flex: 1,
         alignItems: 'center',
-        justifyContent: 'flex-start',
-        paddingTop: 30
-    },
-    top: {
-        flex: 1,
-        alignItems: 'center'
-    },
-    bottom: {
-        flex: 4,
-        width: 200,
-        margin: 3,
-        padding: 10,
-        alignItems: 'center',
+        justifyContent: 'flex-start'
+        // paddingTop: 30
     },
     input: {
-        fontSize: 20,
-        width: 200,
+        minWidth: 300,
         margin: 3,
-        padding: 10
+        padding: 10,
+        // minHeight: 50
     },
+    basicFilters: {
+        flex: 5,
+    },
+    filtersButtons:{
+        padding: 10,
+        margin: 10,
+        alignSelf: 'flex-end'
+    },
+    plusFilter:{
+
+        backgroundColor: '#2b80c4'
+    },
+    minusFilter:{
+        padding: 10,
+        color: '#ff3246'
+    },
+    search:{
+        justifyContent: 'flex-start',
+        alignItems: 'flex-start'
+    }
 });
 
