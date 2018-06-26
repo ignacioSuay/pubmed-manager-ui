7021a673a6a9d4276da8c59784ef077da574c64f
Ignacio Suay <ignacio.suay@gmail.com>
2018-06-26 15:57:25 +0100

    Restyle publication item

--- src/components/PublicationItem/index.js
+++ src/components/PublicationItem/index.js
@@ -1,25 +1,63 @@
 import React from 'react';
-import {Text, TouchableOpacity, View} from "react-native";
+import {StyleSheet, Text, TouchableOpacity, View} from "react-native";
 
 
 export default class PublicationItem extends React.PureComponent {
 
-    _onPress = () => {
-        console.log("Pressed item with id: " + this.props.id);
-        this.props.onPressItem(this.props.id, this.props.item)
-    };
+  _onPress = () => {
+    console.log("Pressed item with id: " + this.props.id);
+    this.props.onPressItem(this.props.id, this.props.item)
+  };
 
-    render() {
-        const textColor = this.props.selected ? "red" : "black";
-        return (
-            <TouchableOpacity onPress={this._onPress}>
-                <View>
-                    <Text style={{ color: textColor }}>
-                        {this.props.title}
-                    </Text>
-                </View>
-            </TouchableOpacity>
-        );
-    }
+  render() {
+    const textColor = this.props.selected ? "red" : "black";
+    const authors = this.props.authors.map(a => a.name).join(", ");
+    return (
+      <TouchableOpacity style={styles.itemContainer} onPress={this._onPress}>
+        <View>
+          <Text style={styles.title}>
+            {this.props.title}
+          </Text>
+          <Text style={styles.authors}>
+            {authors}
+          </Text>
+          <View style={styles.bottom}>
+            <Text style={styles.pubType}>
+              {this.props.type.join(", ")}
+            </Text>
+            <Text style={styles.pubDate}>
+              {this.props.date}
+            </Text>
+          </View>
+        </View>
+      </TouchableOpacity>
+    )
+      ;
+  }
 
 }
+
+
+const styles = StyleSheet.create({
+  itemContainer: {
+    marginTop:5,
+    marginBottom:5,
+  },
+  title: {
+    fontWeight: "bold"
+  },
+  authors: {
+    color: "#6a7989"
+  },
+  bottom:{
+    flexDirection: "row"
+  },
+  pubType: {
+    flex: 1,
+    margin: 2
+  },
+  pubDate: {
+    flex: 1,
+    margin: 2,
+  }
+});
