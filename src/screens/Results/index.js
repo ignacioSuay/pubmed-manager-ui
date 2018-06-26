c57f3e656c231bea0d94b388bbd3a7449982eabf
Ignacio Suay <ignacio.suay@gmail.com>
2018-06-26 15:57:43 +0100

    Fix infinity scroll

--- src/screens/Results/index.js
+++ src/screens/Results/index.js
@@ -1,115 +1,153 @@
 import React from 'react';
-import {Button, FlatList, StyleSheet, Text, View} from 'react-native';
+import {FlatList, StyleSheet, Text, View} from 'react-native';
 import PublicationItem from '../../components/PublicationItem'
 
 export default class Results extends React.PureComponent {
 
-    static navigationOptions = {
-        title: 'Results',
-        headerStyle: {
-            backgroundColor: '#2b80c4'
+  static navigationOptions = {
+    title: 'Results',
+    headerStyle: {
+      backgroundColor: '#2b80c4'
+    }
+  };
+
+  state = {
+    publications: [],
+    selected: {},
+    page: 0,
+    loading: false,
+    numPubs: "",
+    webEnv: null,
+    queryKey: null
+  };
+
+  constructor(props) {
+    super(props);
+  };
+
+  componentDidMount() {
+    const {navigation} = this.props;
+    const term = navigation.getParam('term');
+    this.setState({term: term, page: 0, loading: true}, () => this.fetchData())
+  }
+
+  fetchData = () => {
+    let url = this.buildUrl();
+    fetch(url)
+      .then(response => response.json())
+      .then(responseJson => {
+          const pubs = [...this.state.publications, ...responseJson.publications];
+          this.setState({
+            publications: pubs,
+            loading: false,
+            numPubs: responseJson.count,
+            webEnv: responseJson.webEnv,
+            queryKey: responseJson.queryKey
+          });
         }
-    };
+      )
+      .catch(error => console.log(error));
+  };
 
-    state = {
-        publications: [],
-        selected: {}
-    };
+  buildUrl() {
+    const {term, page} = this.state;
+    const resultsPerPage = 20;
+    const start = (page * resultsPerPage) + 1;
+    let url = 'https://g3ws5fq4m5.execute-api.eu-west-1.amazonaws.com/dev/publications?term=' + term + '&startPage=' + start + '&endPage=' + resultsPerPage;
+    if (this.state.webEnv && this.state.queryKey) {
+      url += `&webEnv=${this.state.webEnv}&queryKey=${this.state.queryKey}`
+    }
+    console.log("fetching: " + term);
+    console.log(url);
+    return url;
+  }
 
-    constructor(props) {
-        super(props);
-    };
+  _renderItem = ({item}) => (
+    <PublicationItem
+      id={item.uid}
+      onPressItem={this._onPressItem}
+      selected={this.state.selected[item.uid]}
+      item={item}
+      title={item.title}
+      authors={item.authors}
+      type={item.pubtype}
+      date={item.pubdate}
+    />
+  );
 
-    fetchData = (term, start, end) => {
-        console.log("fetching" + term);
-        fetch('https://g3ws5fq4m5.execute-api.eu-west-1.amazonaws.com/dev/publications?term=' + term + '&startPage=' + start + '&endPage=' + end)
-            .then(response => response.json())
-            .then(responseJson => this.setState({publications: responseJson}))
-            .catch(error => console.log(error));
-    };
+  _onPressItem = (id, item) => {
+    this.setState((state) => {
+      const selected = state.selected;
+      selected.id = true;
+      return {selected};
+    });
+    console.log("PRESSING id : " + JSON.stringify(id));
+    this.props.navigation.navigate('Details', {id: id, publication: item});
 
-    _renderItem = ({item}) => (
-        <PublicationItem
-            id={item.uid}
-            onPressItem={this._onPressItem}
-            selected={this.state.selected[item.uid]}
-            item={item}
-            title={item.title}
-        />
+  };
+
+  renderSeparator = () => {
+    return (<View style={styles.separator}/>);
+  };
+
+  handleLoadMore() {
+    if (!this.state.loading) {
+      this.setState({page: this.state.page + 1, loading: true}, () => this.fetchData());
+    }
+  }
+
+  render() {
+    const {navigation} = this.props;
+    const term = navigation.getParam('term');
+    console.log("results loading...");
+    return (
+      <View style={styles.container}>
+        <Text>Search term: {term}</Text>
+        <Text>Number results: {this.state.numPubs}</Text>
+        <Text>Loading: {this.state.loading.toString()}</Text>
+
+        <View style={styles.top}>
+          <FlatList
+            data={this.state.publications}
+            renderItem={this._renderItem}
+            keyExtractor={item => item.uid}
+            // extraData={this.state}
+            ItemSeparatorComponent={this.renderSeparator}
+            onEndReached={this.handleLoadMore.bind(this)}
+            onEndReachedThreshold={0.1}
+          />
+        </View>
+      </View>
+
     );
-
-    _onPressItem = (id, item) => {
-        // updater functions are preferred for transactional updates
-        this.setState((state) => {
-            // copy the map rather than modifying state.
-            const selected = state.selected;
-            selected.id = true;
-            return {selected};
-        });
-        console.log("PRESSING id : " + JSON.stringify(id));
-        this.props.navigation.navigate('Details', {id: id, publication: item});
-
-    };
-
-    renderSeparator = () => {
-        return (<View style={styles.separator}/>);
-    };
-
-
-    componentDidMount(){
-        const {navigation} = this.props;
-        const term = navigation.getParam('term');
-        this.fetchData(term, 1, 20);
-    }
-
-    render() {
-        const {navigation} = this.props;
-        const term = navigation.getParam('term');
-        console.log("results loading...");
-        return (
-            <View style={styles.container}>
-                <Text>Search term: {term}</Text>
-
-                <View style={styles.top}>
-                    <FlatList
-                        data={this.state.publications}
-                        renderItem={this._renderItem}
-                        keyExtractor={item => item.uid}
-                        extraData={this.state}
-                        ItemSeparatorComponent={this.renderSeparator}
-                    />
-                </View>
-            </View>
-
-        );
-    }
+  }
 }
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        alignItems: 'center',
-        justifyContent: 'flex-start',
-        paddingTop: 30
-    },
-    top: {
-        flex: 2,
-        alignItems: 'center',
-        margin: 30
-    },
-    bottom: {
-        flex: 1,
-        width: 200,
-        margin: 3,
-        padding: 10
-    },
-    separator: {
-        height: 1,
-        width: "86%",
-        backgroundColor: "#CED0CE",
-        marginLeft: "14%",
-        marginBottom: "2%",
-        marginTop: "2%"
-    }
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'flex-start',
+    paddingTop: 30
+  },
+  top: {
+    flex: 2,
+    alignItems: 'center',
+    margin: 30
+  },
+  bottom: {
+    flex: 1,
+    width: 200,
+    margin: 3,
+    padding: 10
+  },
+  separator: {
+    height: 1,
+    width: "86%",
+    backgroundColor: "#CED0CE",
+    marginLeft: "14%",
+    marginBottom: "2%",
+    marginTop: "2%"
+  }
 });
 
