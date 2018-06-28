7aef1d1d4040940b03e301b06886b66402b78ad5
Ignacio Suay <ignacio.suay@gmail.com>
2018-06-28 12:06:45 +0100

    Load fonts on init

--- src/components/AppContainer/index.js
+++ src/components/AppContainer/index.js
@@ -3,21 +3,38 @@ import Search from '../../screens/Search'
 import Results from '../../screens/Results'
 import Details from '../../screens/Details'
 import {createStackNavigator} from 'react-navigation';
+import { Font, AppLoading } from "expo";
+import FontAwesome from "@expo/vector-icons/fonts/FontAwesome.ttf";
+import MaterialIcons from "@expo/vector-icons/fonts/MaterialIcons.ttf";
+
 
 export default class AppContainer extends React.Component {
 
-    render() {
-        return <RootStack/>;
-    }
+  constructor(props) {
+    super(props);
+    this.state = {loading: true};
+  }
+
+  async componentWillMount() {
+    await Font.loadAsync({
+       FontAwesome,
+      MaterialIcons
+    });
+    this.setState({loading: false});
+  }
+
+  render() {
+    return (!this.state.loading && <RootStack/>);
+  }
 }
 
 const RootStack = createStackNavigator(
-    {
-        Search: Search,
-        Results: Results,
-        Details: Details
-    },
-    {
-        initialRouteName: 'Search',
-    }
+  {
+    Search: Search,
+    Results: Results,
+    Details: Details
+  },
+  {
+    initialRouteName: 'Search',
+  }
 );
