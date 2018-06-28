2e8b36a29017560e3cffa531f3477bfa2403261e
Ignacio Suay <ignacio.suay@gmail.com>
2018-06-28 12:07:01 +0100

    Restyle date picker

--- src/components/DateFilter/index.js
+++ src/components/DateFilter/index.js
@@ -1,50 +1,52 @@
 import React from "react";
-import {Text, View, StyleSheet} from "react-native";
+import {StyleSheet, Text, View} from "react-native";
 import DatePicker from "react-native-datepicker";
 
 
 export default class DateFilter extends React.PureComponent {
 
-    constructor(props) {
-        super(props);
-    };
+  constructor(props) {
+    super(props);
+  };
 
-    render(){
-        const filter = this.props.filter;
-        const dateFrom = this.props.dateFrom;
-        const dateTo = this.props.dateTo;
-        return <View style={styles.dateView}>
-            <Text style={styles.textInput}>{filter.name}</Text>
-            <View style={styles.dateInputView}>
-                <Text style={styles.textInput}>From:</Text>
-                <DatePicker format="YYYY/MM/DD" androidMode="spinner"
-                            date={dateFrom}
-                            placeholder="select date"
-                            onDateChange={date => this.props.onDateChange(filter, date, "from")}/>
-                <Text style={styles.textInput}>To:</Text>
-                <DatePicker format="YYYY/MM/DD" mode="date" androidMode="spinner"
-                            placeholder="Present" date={dateTo}
-                            onDateChange={date => this.props.onDateChange(filter, date, "to")}/>
-            </View>
-        </View>
+  render() {
+    const filter = this.props.filter;
+    const dateFrom = this.props.dateFrom;
+    const dateTo = this.props.dateTo;
+    return <View style={styles.dateView}>
+      <Text style={styles.textInput}>{filter.name}</Text>
+      <View style={styles.dateInputView}>
+        <Text style={styles.textInput}>From:</Text>
+        <DatePicker format="YYYY/MM/DD" androidMode="spinner"
+                    date={dateFrom}
+                    placeholder="select date"
+                    onDateChange={date => this.props.onDateChange(filter, date, "from")}
+                    showIcon={false}/>
+        <Text style={styles.textInput}> To:</Text>
+        <DatePicker format="YYYY/MM/DD" mode="date" androidMode="spinner"
+                    placeholder="Present" date={dateTo}
+                    onDateChange={date => this.props.onDateChange(filter, date, "to")} showIcon={false}/>
+      </View>
+    </View>
 
-    }
+  }
 }
 
 const styles = StyleSheet.create({
-    dateView: {
-        flexDirection: "column",
-        minHeight: 10
-    },
-    dateInputView: {
-        flexDirection: "row",
-    },
-    textInput: {
-        flex: 1,
-        padding: 0,
-        color: '#6a7989',
-        fontSize: 16,
-        margin: 5
-        // fontWeight: 'bold',
-    },
+  dateView: {
+    flexDirection: "column",
+    minHeight: 10
+  },
+  dateInputView: {
+    flexDirection: "row",
+  },
+  textInput: {
+    flex: 1,
+    padding: 0,
+    color: '#6a7989',
+    fontSize: 16,
+    minWidth: 45,
+    fontWeight: 'bold',
+    marginBottom: 5
+  },
 });
