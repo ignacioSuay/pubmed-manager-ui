5d56d7299b832b60970143eae456ecee5144b583
ignacio suay <ignacio.suay@gmail.com>
2018-11-17 19:24:51 +0000

    add redux thunk to handle async actions

--- App.js
+++ App.js
@@ -1,14 +1,15 @@
 import React from 'react';
 import AppContainer from './src/components/AppContainer'
 import reducer from './src/reducers/reducer';
-import {createStore} from 'redux'
+import {applyMiddleware, createStore} from 'redux'
 import {Provider} from 'react-redux';
 import Amplify from 'aws-amplify';
 import aws_exports from './aws-exports';
+import thunk from 'redux-thunk';
 
 
 Amplify.configure(aws_exports);
-const store = createStore(reducer);
+const store = createStore(reducer, applyMiddleware(thunk));
 
 const app = () => (
 
