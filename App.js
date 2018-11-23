cb4677e4a0e7eb75c2ba66ef3f87ef6e5b18f815
ignacio suay <ignacio.suay@gmail.com>
2018-11-23 23:35:55 +0000

    remove aws-amplify

--- App.js
+++ App.js
@@ -3,12 +3,12 @@ import AppContainer from './src/components/AppContainer'
 import reducer from './src/reducers/reducer';
 import {applyMiddleware, createStore} from 'redux'
 import {Provider} from 'react-redux';
-import Amplify from 'aws-amplify';
-import aws_exports from './aws-exports';
+// import Amplify from 'aws-amplify';
+// import aws_exports from './aws-exports';
 import thunk from 'redux-thunk';
 
 
-Amplify.configure(aws_exports);
+// Amplify.configure(aws_exports);
 const store = createStore(reducer, applyMiddleware(thunk));
 
 const app = () => (
